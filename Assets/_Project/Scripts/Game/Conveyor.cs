46ae6155979bc49f7f2ab88646600689f786d10a
busrasevim <busraasevimm@gmail.com>
2025-11-12 05:45:49 +0300

    shooter on conveyor count text

--- Assets/_Project/Scripts/Game/Conveyor.cs
+++ Assets/_Project/Scripts/Game/Conveyor.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Dreamteck.Splines;
+using TMPro;
 using UnityEngine;
 
 namespace _Project.Scripts.Game
@@ -8,9 +10,15 @@ namespace _Project.Scripts.Game
     {
         [SerializeField] private SplineComputer _spline;
         public SplineComputer SplineComputer => _spline;
+        [SerializeField] private TMP_Text shooterCountText;
 
         private int _conveyorLimit;
         private List<Shooter> _shootersOnConveyor = new List<Shooter>();
+
+        private void Start()
+        {
+            SetShooterCountText();
+        }
 
         public bool CanGetNewShooter()
         {
@@ -22,11 +30,13 @@ namespace _Project.Scripts.Game
             if (_shootersOnConveyor.Contains(shooter)) return;
 
             _shootersOnConveyor.Add(shooter);
+            SetShooterCountText();
         }
 
         public void RemoveShooter(Shooter shooter)
         {
             _shootersOnConveyor.Remove(shooter);
+            SetShooterCountText();
         }
 
         public void SetShooterLimit(int shooterLimit)
@@ -37,6 +47,11 @@ namespace _Project.Scripts.Game
         public int GetCurrentShooterCount()
         {
             return _shootersOnConveyor.Count;
+        }
+
+        private void SetShooterCountText()
+        {
+            shooterCountText.text = _shootersOnConveyor.Count.ToString() + "/" + _conveyorLimit;
         }
     }
 }
